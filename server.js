85f55202db6095c010015a637fa267b41dbe5c77
iram <iram@nssplindia.com>
2026-01-03 21:59:19 +0530

    added log in server.js file

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+console.log("ENV CHECK:", {
+  BASE_URL: process.env.BASE_URL,
+  MONGO_URI: !!process.env.MONGO_URI,
+  REDIS_URL: process.env.REDIS_URL
+});
+
+
+
 require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
